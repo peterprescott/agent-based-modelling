bdc0c35e64606fc5a4e20171e095b69c1aa2ec0a
Peter Prescott <peter@prescott.ws>
2019-09-16 14:02:59 +0100

    make x and y variables

---
+++ model.py
@@ -0,0 +1,19 @@
+# Make a y variable.
+
+y0 = 50
+#~ # test y0 has been assigned correctly
+#~ print("y0 = "+str(y0))
+
+
+# Make a x variable.
+
+x0 = 50
+#~ # test x0 has been assigned correctly
+#~ print("x0 = "+str(x0))
+
+# Change y and x based on random numbers.
+
+
+
+# Make a second set of y and xs, and make these change randomly as well.
+# Work out the distance between the two sets of y and xs.
