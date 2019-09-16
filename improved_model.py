22756fc94b59e760e8030ccdf00629040348316c
Peter Prescott <peter@prescott.ws>
2019-09-16 15:41:02 +0100

    iterate with for loops

--- improved_model.py
+++ improved_model.py
@@ -6,70 +6,37 @@ agents = []
 
 num_of_agents = 10
 
+num_of_iterations = 100
+
 # add full number of agents in one smooth move
 
 for i in range(num_of_agents):
     agents.append([random.randint(0,100),random.randint(0,100)])
 
+# move agents
 
-# Change y and x based on random numbers.
+## for each iteration
+for i in range(num_of_iterations):
 
-y0_direction = random.random()
+    ## for each agent
+    for j in range(num_of_agents):
+        y_direction = random.random()
+        x_direction = random.random()
+        if y_direction < 0.5:
+            agents[j][0] += 1
+        else:
+            agents[j][0] -= 1
+        if x_direction < 0.5:
+            agents[j][1] += 1
+        else:
+            agents[j][1] -= 1
 
-if y0_direction < 0.5:
-    agents[0][0] += 1
-else:
-    agents[0][0] -= 1
+matplotlib.pyplot.ylim(-100, 200)
+matplotlib.pyplot.xlim(-100, 200)
 
-x0_direction = random.random()
+for i in range(num_of_agents):
+    matplotlib.pyplot.scatter(agents[i][1],agents[i][0])
 
-if x0_direction < 0.5:
-    agents[0][1] += 1
-else:
-    agents[0][1] -= 1
-
-print(agents[0])
-
-
-# Make a second set of y and xs, and make these change randomly as well.
-
-
-y1_direction = random.random()
-x1_direction = random.random()
-
-if y1_direction < 0.5:
-    agents[1][0] += 1
-else:
-    agents[1][0] -= 1
-    
-if x1_direction < 0.5:
-    agents[1][1] += 1
-else:
-    agents[1][1] -= 1
-    
-print (agents[1])
-
-
-# Work out the distance between the two sets of y and xs.
-
-# Pythagoras: x**2 + y**2 = z**2
-
-distance_squared = (agents[1][1]-agents[0][1])**2 + (agents[0][1]-agents[0][0])**2
-
-distance = distance_squared**(1/2)
-
-print(distance)
-
-# Print Agent with greatest y-value
-print(max(agents, key=operator.itemgetter(0)))
-
-# Print Agent with greatest x-value
-print(max(agents, key=operator.itemgetter(1)))
-
-matplotlib.pyplot.ylim(0, 99)
-matplotlib.pyplot.xlim(0, 99)
-matplotlib.pyplot.scatter(agents[0][1],agents[0][0])
-matplotlib.pyplot.scatter(agents[1][1],agents[1][0])
 
 # Make agent furthest east, ie. greatest x-value, a different colour
 # max(agents, key=operator.itemgetter(1))
