4b780810ec26736bd4bd5a0cbd11da3202b3094f
Peter Prescott <peter@prescott.ws>
2019-09-16 14:22:15 +0100

    find distance between points

--- model.py
+++ model.py
@@ -3,15 +3,10 @@ import random
 # Make a y variable.
 
 y0 = 50
-#~ # test y0 has been assigned correctly
-#~ print("y0 = "+str(y0))
-
 
 # Make a x variable.
 
 x0 = 50
-#~ # test x0 has been assigned correctly
-#~ print("x0 = "+str(x0))
 
 # Change y and x based on random numbers.
 
@@ -33,4 +28,32 @@ print(x0,y0)
 
 
 # Make a second set of y and xs, and make these change randomly as well.
+
+y1 = 50
+x1 = 50
+
+y1_direction = random.random()
+x1_direction = random.random()
+
+if y1_direction < 0.5:
+    y1 += 1
+else:
+    y1 -= 1
+    
+if x1_direction < 0.5:
+    x1 += 1
+else:
+    x1 -= 1
+    
+print (x1, y1)
+
+
 # Work out the distance between the two sets of y and xs.
+
+# Pythagoras: x**2 + y**2 = z**2
+
+distance_squared = (x1-x0)**2 + (y1-y0)**2
+
+distance = distance_squared**(1/2)
+
+print(distance)
