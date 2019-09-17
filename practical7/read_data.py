b0d4820c12c61afe97c7d6cfd601782c4408ab74
Peter Prescott <peter@prescott.ws>
2019-09-17 16:54:15 +0100

    copy files to new folder for practical7

---
+++ practical7/read_data.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot
+import csv
+
+## https://www.geog.leeds.ac.uk/courses/computing/study/core-python-phd/part7/index.html
+environment = []
+
+dataset = open('in.txt', newline='')
+reader = csv.reader(dataset)
+for row in reader:
+    rowlist = []
+    for numeric_string in row:
+        value = int(numeric_string)
+        rowlist.append(value)
+    environment.append(rowlist)
+dataset.close() 
+
+matplotlib.pyplot.imshow(environment)
+matplotlib.pyplot.show()
