9fec454fbd5a3298b7b75dc703224321048f1530
Peter Prescott <peter@prescott.ws>
2019-09-17 10:09:15 +0100

    start neatly structuring model

---
+++ neat_model.py
@@ -0,0 +1,20 @@
+
+# import the libraries
+
+
+# define pythagorean distance calculator
+
+
+# set variables
+
+
+# make the agents
+
+
+# move the agents
+
+
+# plot the agents
+
+
+# measure distance between the agents
