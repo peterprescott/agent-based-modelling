840624c5576bd056e03cc41349ce0b2b685aa5de
Prescott <gypiwp@liverpool.ac.uk>
2019-10-25 12:55:08 +0100

    update tests to include extra cmd_line parameter

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,7 +1,7 @@
-python ../run_model.py 10 10 100
+python ../run_model.py 40 50 10 100
 
-python ../run_model.py 10 10 1000
+python ../run_model.py 40 50 10 1000
 
-python ../run_model.py 10 10 10000
+python ../run_model.py 40 50 10 10000
 
-python ../run_model.py 10 10 100000
+python ../run_model.py 40 50 10 100000
